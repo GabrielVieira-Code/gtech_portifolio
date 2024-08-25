eb8cf8496cca5d76bde670d677cac791e286c1ef
gabrielvieira-code <gabrielvieira.code@gmail.com>
2024-08-25 19:00:17 -0300

    Estilizaçao da pagina home finalizada

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,27 @@ export default function Home() {
     <>
       <Navbar />
       <div className="flex h-screen">
-        <div className="w-2/5 p-4 flex flex-col justify-center">
-          <h1 className="text-2xl font-bold mb-4">Título Aqui</h1>
-          <p className="text-lg">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet accumsan arcu. Nulla facilisi. Nullam egestas ligula in ex hendrerit, nec consequat lorem sodales.
-          </p>
+        {/* Contêiner para o texto */}
+        <div className="w-full md:w-2/5 flex flex-col justify-center relative p10 md:pt-25 md:pb-20 md:px-8 min-h-screen">
+          <div className="flex flex-col justify-start">
+            <h1 className="text-2xl font-bold mb-4">Seja bem-vindo ao Portfólio Gtech</h1>
+            <p className="text-lg">
+              Sou analista e desenvolvedor de sistemas, graduado em análise e desenvolvimento de sistemas pela UNIP. Iniciei minha carreira como suporte técnico e posteriormente atuei como desenvolvedor.
+              Tenho conhecimentos em Node, React, HTML, CSS, Java, AWS.
+            </p>
+          </div>
         </div>
-        <div className="w-3/5 relative">
-        <Image
-  src="/imagens/HomeIMG.png" // Caminho correto
-  alt="Descrição da Imagem"
-  layout="fill"
-  objectFit="cover"
-/>
-
+        {/* Contêiner para a imagem */}
+        <div className="w-full md:w-3/5 relative flex items-center justify-center">
+          <div className="relative w-full h-full">
+            <Image
+              src="/imagens/HomeIMG.png"
+              alt="Descrição da Imagem"
+              layout="fill"
+              objectFit="cover"
+              className="object-cover"
+            />
+          </div>
         </div>
       </div>
       <Footer />
